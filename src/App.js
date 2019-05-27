accef1c7dd009f2f335470a6393fc53b10aba88b
Guilherm Santos <gui07015@gmail.com>
2019-05-27 11:50:26 -0300

    Import GlobalStyles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from "react";
-import { Provider } from "react-redux";
+import React from 'react';
+import { Provider } from 'react-redux';
 
-import store from "./store";
+import store from './store';
 
-import Home from "./pages/home";
+import Home from './pages/home';
+import GlobalStyle from './styles/globalStyle';
 
 const App = () => (
-  <Provider store={store}>
-    <Home />
-  </Provider>
+  <>
+    <Provider store={store}>
+      <GlobalStyle />
+      <Home />
+    </Provider>
+  </>
 );
 export default App;
