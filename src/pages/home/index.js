c00400db36e1c302d785bc5ba4ee234cdbad306d
Guilherm Santos <gui07015@gmail.com>
2019-05-27 13:47:40 -0300

    Updated

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,26 +1,23 @@
 import React from 'react';
 
-import {
-  Container, Nav, CardLogin, Login,
-} from './styles';
+import Header from '../../components/header';
+
+import { Container, CardLogin, Login } from './styles';
 
 const Home = () => (
-  <Container>
-    <Nav>
-      <a href="">Home</a>
-      <a href="">About</a>
-      <a href="">Sign in</a>
-      <a href="">Login</a>
-    </Nav>
+  <>
+    <Container>
+      <Header />
 
-    <CardLogin>
-      <Login>
-        <p>Hello, enter your github user</p>
-        <input type="text" placeholder="ex: Guirds" />
-        <button type="submit">Send</button>
-      </Login>
-    </CardLogin>
-  </Container>
+      <CardLogin>
+        <Login>
+          <p>Hello, enter your github user</p>
+          <input type="text" placeholder="ex: Guirds" />
+          <button type="submit">Send</button>
+        </Login>
+      </CardLogin>
+    </Container>
+  </>
 );
 
 export default Home;
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -7,29 +7,6 @@ export const Container = styled.div`
   align-items: center;
 `;
 
-export const Nav = styled.nav`
-  width: 100%;
-  display: flex;
-  justify-content: space-around;
-  padding: 0;
-
-  a {
-    width: 100%;
-    padding: 20px;
-    border-radius: 3px;
-
-    color: #bcbacb;
-    font-size: 20px;
-    text-align: center;
-    text-decoration-line: none;
-
-    &:hover {
-      color: #6c757d;
-      background: #37e8a0;
-    }
-  }
-`;
-
 export const CardLogin = styled.div`
   display: flex;
   justify-content: center;
