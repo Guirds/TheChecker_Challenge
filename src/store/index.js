7f72c2daa19aca67bc8ffe7881fd070b514e9625
Guilherm Santos <gui07015@gmail.com>
2019-05-28 01:45:39 -0300

    Config  SagaMonitor and Saga Middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,16 @@
 import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
-import reducers from './reducers/index';
+import reducers from './ducks';
+import sagas from './sagas';
 
 const middlewares = [];
 
-middlewares.push();
+const sagaMonitor = process.env.NODE_ENV === 'development' ? console.tron.createSagaMonitor() : null;
+
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+
+middlewares.push(sagaMiddleware);
 
 const composer = process.env.NODE_ENV === 'development'
   ? compose(
@@ -14,5 +20,7 @@ const composer = process.env.NODE_ENV === 'development'
   : compose(applyMiddleware(...middlewares));
 
 const store = createStore(reducers, composer);
+
+sagaMiddleware.run(sagas);
 
 export default store;
