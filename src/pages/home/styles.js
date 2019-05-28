1cd56cf0d053e0e07cac8e88e3b8ae93202e9ad2
Guilherm Santos <gui07015@gmail.com>
2019-05-28 07:52:33 -0300

    Updated

--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -5,47 +5,23 @@ export const Container = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-`;
-export const Nav = styled.nav`
-  width: 100%;
-  display: flex;
-  justify-content: space-around;
-  padding: 0;
-  a {
-    width: 100%;
-    padding: 20px;
-    border-radius: 3px;
-    color: #bcbacb;
-    font-size: 20px;
-    text-align: center;
-    text-decoration-line: none;
-    &:hover {
-      color: #6c757d;
-      background: #37e8a0;
-    }
-  }
-`;
-
-export const CardLogin = styled.div`
-  display: flex;
-  justify-content: center;
-
-  margin-top: 15%;
-  width: 25%;
-  height: 200px;
-  border-radius: 5px;
-
-  box-shadow: 0px 2px 4px 0 rgb(2, 211, 124);
-
-  &:hover {
-    box-shadow: 0 10px 20px 0 rgb(55, 232, 160);
-  }
 
   form {
     display: flex;
     align-items: center;
     flex-direction: column;
     justify-content: space-around;
+
+    margin-top: 15%;
+    width: 25%;
+    height: 200px;
+    border-radius: 5px;
+
+    box-shadow: 0px 2px 4px 0 rgb(2, 211, 124);
+
+    &:hover {
+      box-shadow: 0 10px 20px 0 rgb(55, 232, 160);
+    }
 
     p {
       color: #6c757d;
@@ -83,6 +59,26 @@ export const CardLogin = styled.div`
       &:hover {
         width: 120px;
       }
+    }
+  }
+`;
+
+export const Nav = styled.nav`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  padding: 0;
+  a {
+    width: 100%;
+    padding: 20px;
+    border-radius: 3px;
+    color: #bcbacb;
+    font-size: 20px;
+    text-align: center;
+    text-decoration-line: none;
+    &:hover {
+      color: #6c757d;
+      background: #37e8a0;
     }
   }
 `;
