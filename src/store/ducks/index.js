aba4280c0d43263047f0e83ba7d1c74e9558e264
Guilherm Santos <gui07015@gmail.com>
2019-05-28 01:44:37 -0300

    Updated

---
+++ src/store/ducks/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+
+import repository from './repository';
+
+export default combineReducers({
+  // Remova essa linha depois de adicionar seus ducks
+  repository,
+});
